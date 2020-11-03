08e262cf1956a3702d0db5223030f1607553410e
J2b2590 <J2b2590@gmail.com>
2020-11-03 08:42:01 -0600

    finished lab

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+testFunc()
+
+
+function superbowlWin(w){
+  
+  let result = w.find( w => w.result === "W" )
+  return !!result ? result.year : undefined
+  
+  
+}
